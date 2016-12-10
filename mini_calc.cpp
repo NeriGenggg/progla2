dd8a6ffc700832334a3dc7b9bbe7ebacaa3c3884
Nerissa Ann B. Dumaguila <nerissadumaguila0412@yahoo.com>
2016-12-10 18:06:40 +0800

    Create mini_calc.cpp

---
+++ mini_calc.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+
+
+int get_sum(int x, int y){
+	return x+y;
+}
+
+
+int get_difference(int x, int y){
+	return x-y;
+}
+
+int get_product(int x, int y){
+	return x*y;
+}
+
+
+int get_quotient(int x, int y){
+	return x/y;
+}
+
+
+
+int main(){
+	int x, y;
+	
+	cout<<"Enter num 1:";
+	cin>>x;
+	cout<<"Enter num 2:";
+	cin>>y;
+	
+	cout<<"Sum is:"<<get_sum(x, y)<<endl;
+	cout<<"Difference is:"<<get_difference(x,y)<<endl;
+	cout<<"Product is:"<<get_product(x,y)<<endl;
+	cout<<"Quotient is:"<<get_quotient( x,y);
+	
+	return 0;
+}
