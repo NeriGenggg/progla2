de0679e9d4f0129f7fcf018db90c1ca34e6e4d10
Nerissa Ann B. Dumaguila <nerissadumaguila0412@yahoo.com>
2016-12-10 18:20:16 +0800

    Update mini_calc.cpp

--- mini_calc.cpp
+++ mini_calc.cpp
@@ -1,13 +1,9 @@
 #include<iostream>
 using namespace std;
 
-
-
 int get_sum(int x, int y){
 	return x+y;
 }
-
-
 int get_difference(int x, int y){
 	return x-y;
 }
@@ -15,26 +11,22 @@ int get_difference(int x, int y){
 int get_product(int x, int y){
 	return x*y;
 }
-
-
 int get_quotient(int x, int y){
 	return x/y;
 }
 
-
-
 int main(){
 	int x, y;
 	
-	cout<<"Enter num 1:";
+	cout<<"Enter num 1: ";
 	cin>>x;
-	cout<<"Enter num 2:";
+	cout<<"Enter num 2: ";
 	cin>>y;
 	
-	cout<<"Sum is:"<<get_sum(x, y)<<endl;
-	cout<<"Difference is:"<<get_difference(x,y)<<endl;
-	cout<<"Product is:"<<get_product(x,y)<<endl;
-	cout<<"Quotient is:"<<get_quotient( x,y);
+	cout<<"Sum is: "<<get_sum(x, y)<<endl;
+	cout<<"Difference is: "<<get_difference(x,y)<<endl;
+	cout<<"Product is: "<<get_product(x,y)<<endl;
+	cout<<"Quotient is: "<<get_quotient( x,y);
 	
 	return 0;
 }
