9dde17d4280d23cea39cf8428fa6a11d5753d33d
Nerissa Ann B. Dumaguila <nerissadumaguila0412@yahoo.com>
2016-12-17 17:25:53 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -6,26 +6,24 @@ struct Person{
 	int shoulder;
 	int head;
 }; 
-int main()
-{
-	
+int main(){
 	Person person;
 	for (int i;i<5;i++){
-	
+	cout << "Person number " << (i+1) << endl;
 	cout << "Person's toes:" << endl;
-	 cin >> person.toes;
-	 cout << "Person's knees:" << endl;
-	 cin >> person.knees;
-	 cout << "Person's shoulder:" << endl;
-	 cin >> person.shoulder;
-	 cout << "Person's head:" << endl;
-	 cin >> person.head;
-	 cout <<"Toes: " << person.toes << endl;
-	 cout <<"knees: " << person.knees << endl;
-	 cout <<"shoulder: " << person.shoulder << endl;
-	 cout << "head: " <<person.head << endl;
-	 system("pause"); 
-	 system("CLS"); 
+	cin >> person.toes;
+	cout << "Person's knees:" << endl;
+	cin >> person.knees;
+	cout << "Person's shoulder:" << endl;
+	cin >> person.shoulder;
+	cout << "Person's head:" << endl;
+	cin >> person.head;
+	cout <<"Toes: " << person.toes << endl;
+	cout <<"knees: " << person.knees << endl;
+	cout <<"shoulder: " << person.shoulder << endl;
+	cout << "head: " <<person.head << endl;
+	system("pause"); 
+	system("CLS"); 
 }
 	return 0;
 }
