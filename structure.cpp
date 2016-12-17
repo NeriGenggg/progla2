770818dcbd94056487006547cc6a34457dd71a4c
Nerissa Ann B. Dumaguila <nerissadumaguila0412@yahoo.com>
2016-12-17 16:48:23 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -6,4 +6,25 @@ struct Person{
 	int knees;
 	int shoulder;
 	int head;
-};
+}; 
+int main()
+{
+	
+	Person person;
+	cout << "Person's toes:" << endl;
+	 cin >> person.toes;
+	 cout << "Person's knees:" << endl;
+	 cin >> person.knees;
+	 cout << "Person's shoulder:" << endl;
+	 cin >> person.shoulder;
+	 cout << "Person's head:" << endl;
+	 cin >> person.head;
+	 
+	 cout <<"Toes: " << person.toes << endl;
+	 cout <<"knees: " << person.knees << endl;
+	 cout <<"shoulder: " << person.shoulder << endl;
+	 cout << "head: " <<person.head << endl;
+	return 0;
+}
+
+	
