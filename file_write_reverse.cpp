b21ebf7606dce9c794e866337ef228e669e92d4c
Nerissa Ann B. Dumaguila <nerissadumaguila0412@yahoo.com>
2017-01-21 16:47:15 +0800

    Update file_write_reverse.cpp

--- file_write_reverse.cpp
+++ file_write_reverse.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+    FILE *myFile;
+    int numberArray[5];
 
+    for(int i = 0 ; i < 5 ; i++){
+		cin >> numberArray[i];
+	}
+    myFile = fopen("sample1.txt", "w");
+    for (int i = 4; i >= 0; i--){
+        fprintf(myFile, "%d\n", numberArray[i]);
+    }
+	fclose(myFile);
+
+	return 0;
+}
